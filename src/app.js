17d932192f28a3dfc72e8cc2cd37b3f449c4799c
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-07-09 10:24:29 +0530

    Refactor CORS configuration t

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ import path from 'path';
 const app = express(); 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true              
+  origin: [
+    'http://localhost:5173',//local
+    'https://ola-frontend-dun.vercel.app'// vercel app
+  ],
+  credentials: true
 }));
+
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
